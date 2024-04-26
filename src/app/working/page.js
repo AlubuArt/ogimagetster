776540024aff193ca61a3c14f7dda193affce1c8
Jacob <lz77uc@its.aau.dk>
2024-04-26 10:21:54 +0200

    full og

--- src/app/working/page.js
+++ src/app/working/page.js
@@ -15,7 +15,14 @@ export const metadata = {
   title: "Create Next App",
   description: "Gsomething",
   openGraph: {
-    images: [ 'https://prod-aaudxp-cms-001-app.azurewebsites.net/media/ubdgjwfl/aau-energy-2.png'],
+    images: [{ url:'https://www.aaudxp-cms.aau.dk/media/0s1dkbzr/aau_bygninger_090719_00398_effect-800px.jpg', width: 1200, height: 630}],
+    url: 'https://ogimagetster.vercel.app/',
+    type: 'website',
+    title: 'Test OG',
+    description: 'test site',
+    site_name: 'ogimagetster.vercel.app',
+
+
   },
   twitter: {
     card: "summary",
