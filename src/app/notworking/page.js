dcff7f706ef11ef2da19a351a42ffc0cfd7d9318
Jacob <lz77uc@its.aau.dk>
2024-04-26 07:15:16 +0200

    testing twitter card

--- src/app/notworking/page.js
+++ src/app/notworking/page.js
@@ -7,9 +7,9 @@ export const metadata = {
     } 
 
 export default function Home() {
-    return (
-    
 
+
+    return (
        <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                     <p>not working OG image: https://prod-aaudxp-cms-001-app.azurewebsites.net/media/ubdgjwfl/aau-energy-2.png</p>
--- src/app/working/page.js
+++ src/app/working/page.js
@@ -1,18 +1,27 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p>
+          working OG image:
+          https://www.aaudxp-cms.aau.dk/media/ubdgjwfl/aau-energy-2.png
+        </p>
+      </div>
+    </main>
+  );
+}
+
 export const metadata = {
   title: "Create Next App",
   description: "Gsomething",
   openGraph: {
-images: 'https://www.aaudxp-cms.aau.dk/media/ubdgjwfl/aau-energy-2.png'
-}
-  } 
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <p>working OG image: https://www.aaudxp-cms.aau.dk/media/ubdgjwfl/aau-energy-2.png</p>
-        </div>
-      </main>
-  );
-}
-
+    images: [ 'https://www.aaudxp-cms.aau.dk/media/ubdgjwfl/aau-energy-2.png'],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Your title",
+    description: "Your description",
+    creator: "@author_name",
+    images: ["https://prod-aaudxp-cms-001-app.azurewebsites.net/media/ubdgjwfl/aau-energy-2.png"],
+  },
+};
